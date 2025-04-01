25df5805c260726417288518fda0f07c7f5c3529
Carolina Zepeda <mirian.hernandez1@catolica.edu.sv>
2025-04-01 17:17:15 -0600

    Add project files.

---
+++ L02P02_2022CP602_2022HZ651/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022CP602_2022HZ651.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ L02P02_2022CP602_2022HZ651/Models/LibreriaContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L02P02_2022CP602_2022HZ651.Models
+{
+    public class LibreriaContext : DbContext
+    {
+    }
+}
