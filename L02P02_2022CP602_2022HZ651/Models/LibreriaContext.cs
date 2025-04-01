af13d163537325da117d06e4e34dc22f5fc98b67
Carolina Zepeda <mirian.hernandez1@catolica.edu.sv>
2025-04-01 17:29:16 -0600

    Enviando atualizacion del conetxt

--- L02P02_2022CP602_2022HZ651/Models/LibreriaContext.cs
+++ L02P02_2022CP602_2022HZ651/Models/LibreriaContext.cs
@@ -4,5 +4,15 @@ namespace L02P02_2022CP602_2022HZ651.Models
 {
     public class LibreriaContext : DbContext
     {
+        public LibreriaContext(DbContextOptions<LibreriaContext> options) : base(options)
+        {
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<PedidoEncabezado> PedidosEncabezado { get; set; }
+        public DbSet<PedidoDetalle> PedidosDetalle { get; set; }
+        public DbSet<ComentarioLibro> ComentariosLibros { get; set; }
     }
 }
