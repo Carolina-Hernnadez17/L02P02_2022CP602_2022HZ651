1f3d33af62256c2fa65e262280a8640e9139396c
Carolina Zepeda <mirian.hernandez1@catolica.edu.sv>
2025-04-01 23:53:43 -0600

    enviando cambios del layout

--- L02P02_2022CP602_2022HZ651/Program.cs
+++ L02P02_2022CP602_2022HZ651/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Venta}/{action=IniciarVenta}/{id?}");
 
 app.Run();
